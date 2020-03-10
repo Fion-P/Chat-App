03ae775cd49fa7a933f06c897c00446d87f0f484
Fion-P <fionpang97@gmail.com>
2020-03-09 23:48:38 -0700

    generate user table

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :username, :email
+end
