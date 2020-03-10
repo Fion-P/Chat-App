daf1b5563558896b57cfee41018f91b4e81c7e62
Fion-P <fionpang97@gmail.com>
2020-03-10 00:00:18 -0700

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,46 @@
 #
 
 class User < ApplicationRecord
-  validates :username, :email
+
+  attr_reader :password
+
+  validates :username, :email, uniqueness: true, presence: true
+  validates :first_name, :last_name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    # find the user by the username
+    user = User.find_by(username: username)
+
+    # check is user exists and if so if the password is correct
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    # use BCrypt to create password digest
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    # Password.new reverses password digest to the password 
+    # is_password in the line below is a Bcrypy method to check if password is correct
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    # use Secure Random to create a new session token
+    self.update!(session_token: SecureRandom.urlsafe_base64)
+    # return the updated session token
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    # lazy initialization for the session token if one isn't there
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
