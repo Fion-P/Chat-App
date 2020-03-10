7edb9081e279aa420e57177b0cdcbe75612d03ed
Fion-P <fionpang97@gmail.com>
2020-03-10 13:57:34 -0700

    create session actions

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,7 +4,7 @@ import * as sessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hit");
-  window.sessionAPIUtil = sessionAPIUtil;
+  // window.sessionAPIUtil = sessionAPIUtil;
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to ChatApp</h1>, root);
 });
