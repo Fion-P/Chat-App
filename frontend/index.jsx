49336baba2ec7fea52c702d4479ad87a8e7ad6d3
Fion-P <fionpang97@gmail.com>
2020-03-10 14:26:51 -0700

    create reducers, store, and base components

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as sessionAPIUtil from "./util/session_api_util";
+import configureStore from "./store/store";
+// import Root from "./components/root";
+import Root from "./components/root";
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hit");
-  // window.sessionAPIUtil = sessionAPIUtil;
+
+  const store = configureStore();
+  // we don't put the store directly on the window because
+  // it can be confusing when debugging, sometimes giving you access to state
+  // when you shouldn't
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to ChatApp</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,3 +1,4 @@
+
 export const signup = user => {
   return $.ajax({
     method: "POST",
