4ba11e8be8abcd679e29e07e42be9d46a6ec93d5
Fion-P <fionpang97@gmail.com>
2020-03-12 21:21:57 -0700

    add error messages to signup page

--- frontend/components/session-forms/signup.jsx
+++ frontend/components/session-forms/signup.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { withRouter, Link } from "react-router-dom";
 
 class Signup extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       first_name: "",
       last_name: "",
       username: "",
-      password: "",
+      password: ""
     };
 
     this.handleInput = this.handleInput.bind(this);
@@ -23,17 +22,43 @@ class Signup extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
-    this.props.signup(user)
-      .then(res => this.props.history.push(`/${res.currentUser.id}`));
+    this.props.signup(user).then(
+      res => this.props.history.push(`/${res.currentUser.id}`),
+      () => this.handleErrors()
+    );
+  }
+
+  handleErrors() {
+    // console.log(this.props.errors.session.responseJSON);
+    let err = this.props.errors.session.responseText;
+    // let err = this.props.errors.join("");
+    this.checkError("First", ".input-fn", err, ".input-fn-err");
+    this.checkError("Last", ".input-ln", err, ".input-ln-err");
+    this.checkError("Username", ".input-uname", err, ".input-uname-err");
+    this.checkError("Password", ".input-pw", err, ".input-pw-err");
+  }
+
+  checkError(param, htmlClass, err, errMsg) {
+    if (err.includes(param)) {
+      // document.querySelector(htmlClass).classList.add("red-bord");
+      document.querySelector(htmlClass).style.borderColor = "#d93125e3";
+      document.querySelector(errMsg).style.display = "block";
+    } else {
+      // document.querySelector(htmlClass).classList.remove("red-bord");
+      document.querySelector(htmlClass).style.borderColor = "#bdbdbdaf";
+      document.querySelector(errMsg).style.dispay = "none";
+    }
   }
 
   render() {
     let errors = this.props.errors.session.responseJSON || [];
 
     return (
-      <div className="signup-container"> 
+      <div className="signup-container">
         <div className="signup-header">
-          <Link to="/"><img src="favicon.png" className="signup-icon" /></Link>
+          <Link to="/">
+            <img src="favicon.png" className="signup-icon" />
+          </Link>
           <h1 className="signup-title">Messenger</h1>
           <h2 className="sigup-title-h2">
             Connect with friends and the world around you on Facebook.
@@ -44,51 +69,63 @@ class Signup extends React.Component {
             <div className="signup-input-container">
               <input
                 name="first_name"
-                className="signup-input"
+                className="signup-input input-fn"
                 type="text"
                 value={this.state.first_name}
                 onChange={() => this.handleInput("first_name")}
                 placeholder="First Name"
               />
-              <label className="signup-input-label" htmlFor="first_name">First Name</label>
+              <label className="signup-input-label" htmlFor="first_name">
+                First Name
+              </label>
+              <h2 className="input-fn-err">First name can't be blank</h2>
             </div>
 
             <div className="signup-input-container">
               <input
                 name="last_name"
-                className="signup-input"
+                className="signup-input input-ln"
                 type="text"
                 value={this.state.last_name}
                 onChange={() => this.handleInput("last_name")}
                 placeholder="Last Name"
                 autoComplete="on"
               />
-              <label className="signup-input-label" htmlFor="last_name">Last Name</label>
+              <label className="signup-input-label" htmlFor="last_name">
+                Last Name
+              </label>
+              <h2 className="input-ln-err">Last name can't be blank</h2>
             </div>
 
             <div className="signup-input-container">
               <input
                 name="username"
-                className="signup-input"
+                className="signup-input input-uname"
                 type="text"
                 value={this.state.username}
                 onChange={() => this.handleInput("username")}
                 placeholder="Username"
               />
-              <label className="signup-input-label" htmlFor="username">Username</label>
+              <label className="signup-input-label" htmlFor="username">
+                Username
+              </label>
+              <h2 className="input-uname-err">Username can't be blank</h2>
             </div>
 
             <div className="signup-input-container">
               <input
                 name="password"
                 type="password"
-                className="signup-input"
+                className="signup-input input-pw"
                 value={this.state.password}
                 onChange={() => this.handleInput("password")}
                 placeholder="Password"
                 autoComplete="on"
               />
-              <label className="signup-input-label" htmlFor="password">Password</label>
+              <label className="signup-input-label" htmlFor="password">
+                Password
+              </label>
+              <h2 className="input-pw-err">Password can't be blank</h2>
             </div>
 
             <div className="session-form-redirect">
@@ -107,25 +144,35 @@ class Signup extends React.Component {
         </div>
         <div className="singup-filler-bottom">
           <span className="signup-flink">
-            <a href="https://github.com/Fion-P/Chat-App" target="_blank">Github</a>
+            <a href="https://github.com/Fion-P/Chat-App" target="_blank">
+              Github
+            </a>
           </span>
           <span className="signup-spacer">|</span>
           <span className="signup-flink">
-            <a href="http://fion-pang.com/" target="_blank">Portfolio</a>
+            <a href="http://fion-pang.com/" target="_blank">
+              Portfolio
+            </a>
           </span>
           <span className="signup-spacer">|</span>
           <span className="signup-flink">
-            <a href="https://www.linkedin.com/in/fion-pang-429172154/" target="_blank">LinkedIn</a>
+            <a
+              href="https://www.linkedin.com/in/fion-pang-429172154/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
           </span>
           <span className="signup-spacer">|</span>
           <span className="signup-flink">
-            <a href="https://angel.co/fion-pang-1" target="_blank">AngelList</a>
+            <a href="https://angel.co/fion-pang-1" target="_blank">
+              AngelList
+            </a>
           </span>
         </div>
       </div>
-    )
+    );
   }
-
 }
 
 export default withRouter(Signup);
