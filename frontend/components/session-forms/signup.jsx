54b75585e32857c5c325b46ad973b7bbb64bf413
Fion-P <fionpang97@gmail.com>
2020-03-13 01:10:13 -0700

    change mobile styling for err messages

--- frontend/components/session-forms/signup.jsx
+++ frontend/components/session-forms/signup.jsx
@@ -41,20 +41,21 @@ class Signup extends React.Component {
 
   checkError(param, htmlClass, err, errMsg) {
     let errors = this.props.errors.session.responseJSON || [];
-    console.log(errors);
+
     if (err.includes(param)) {
       // document.querySelector(htmlClass).classList.add("red-bord");
       document.querySelector(htmlClass).style.borderColor = "#d93125e3";
       document.querySelector(errMsg).style.display = "block";
 
-      // if (param === "Username") {
+      if (param === "Username" || param === "Password") {
         errors.forEach( err => {
           if (err.includes(param)) {
             document.querySelector(errMsg).innerHTML = err;
             // break;
           }
         });
-      // }
+      }
+      
     } else {
       // document.querySelector(htmlClass).classList.remove("red-bord");
       document.querySelector(htmlClass).style.borderColor = "#bdbdbdaf";
