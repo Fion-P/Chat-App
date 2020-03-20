e93e8f076c886be1b612b8adb5cb3303488d46c3
Fion-P <fionpang97@gmail.com>
2020-03-19 19:20:27 -0700

    create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_224102) do
+ActiveRecord::Schema.define(version: 2020_03_20_021723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2020_03_13_224102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chatroom_id", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "pofile_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
