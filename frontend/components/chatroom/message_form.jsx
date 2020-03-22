4e0cc07368bb439e664c9a8d308f4622b6d47819
Fion-P <fionpang97@gmail.com>
2020-03-22 16:00:08 -0700

    add emojis

--- frontend/components/chatroom/message_form.jsx
+++ frontend/components/chatroom/message_form.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+// import React, { useState } from "react";
+import Picker from "emoji-picker-react";
 
 class MessageForm extends React.Component {
 
@@ -7,9 +9,13 @@ class MessageForm extends React.Component {
     super(props);
     this.state = {
       body: "",
+      emoji: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleEmoji = this.toggleEmoji.bind(this);
+    this.onEmojiClick = this.onEmojiClick.bind(this);
+    this.closeEmoji = this.closeEmoji.bind(this);
   }
 
   update(field) {
@@ -34,7 +40,7 @@ class MessageForm extends React.Component {
 
         }
       );
-      this.setState({ body: "" });
+      this.setState({ body: "", emoji: false });
     }
 
     // let chats = document.querySelector('.chats');
@@ -42,23 +48,56 @@ class MessageForm extends React.Component {
     // chats.scrollTop = chats.scrollHeight;
   }  
 
+  onEmojiClick(e, emojiObject) {
+    let body = this.state.body;
+    body += emojiObject.emoji;
+    
+    // console.log(body);
+    this.setState({body: body});
+  }
+
+  toggleEmoji() {
+    let shown = this.state.emoji;
+    this.setState({emoji: !shown});
+  }
+
+  closeEmoji() {
+    this.setState({ emoji: false });
+  }
+
   render() {
       
     // console.log(this.state);
 
+    let emojiClass = "";
+    if (!this.state.emoji) {
+      emojiClass = "emoji-hidden";
+    }
+
     return (
       <div className="chat-form">
         <form className="message-form" onSubmit={this.handleSubmit}>
-          <input className="chat-input" autoFocus
+          <input
+            className="chat-input"
+            autoFocus
             type="text"
             value={this.state.body}
             onChange={this.update("body")}
+            onClick={this.closeEmoji}
             placeholder="Type a message..."
           />
           <input type="submit" value="" />
         </form>
+        <div className="emoji-container">
+          <div className="emoji-toggle" onClick={this.toggleEmoji}>
+            <i className="far fa-grin"></i>
+          </div>
+          <div className={`emoji-picker-container ${emojiClass}`}>
+            <Picker onEmojiClick={this.onEmojiClick} />
+          </div>
+        </div>
       </div>
-    )
+    );
 
   }
 }
