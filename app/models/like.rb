6ee12f4c8bb685480335ab6042f6bd1f549475ea
Fion-P <fionpang97@gmail.com>
2020-03-29 23:44:55 -0700

    add likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  post_id    :integer          not null
+#  post_type  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Like < ApplicationRecord
+  
 end
