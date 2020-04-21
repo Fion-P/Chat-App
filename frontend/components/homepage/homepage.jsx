f4df1e3b95686837552bc8bdfae3d892da29166c
Fion-P <fionpang97@gmail.com>
2020-04-20 20:53:42 -0700

    fix homepage photos

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -11,7 +11,7 @@ export default () => {
       <div className="h-content1">
         < Login /> 
         <div className="splash-photo-container">
-          <img className="splash-photo" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/88164652_831059540702416_4401096152632524800_n.png?_nc_cat=1&_nc_sid=6825c5&_nc_ohc=tPFc1e3w9DcAX8jpmdr&_nc_ht=scontent-sjc3-1.xx&oh=9e9bc8d2deee0ee862b2f265e3218bc9&oe=5E90700D" alt=""/>
+          <img className="splash-photo" src="homepage1.png" alt=""/>
         </div>
       </div>
       <div className="h-content2">
@@ -20,7 +20,7 @@ export default () => {
           <div className="c2-block-container">
             <div className="c2-block">
               <div className="block-img">
-                <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/42466073_1965890656806201_8160997823056183296_n.png?_nc_cat=1&_nc_sid=6825c5&_nc_ohc=ORBIixlO2b8AX-PHHAn&_nc_ht=scontent-sjc3-1.xx&oh=f5e95ab5d445905f6f19f3a46447cb41&oe=5E935F2C" 
+                <img src="homepage2.png" 
                 />
               </div>
               <div className="c2-block-content">
@@ -36,7 +36,7 @@ export default () => {
           <div className="c2-block-container">
             <div className="c2-block">
               <div className="block-img">
-                <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/42428744_249359745772029_7507114388252262400_n.png?_nc_cat=1&_nc_sid=6825c5&_nc_ohc=k0lVqpVaXKwAX8PTaew&_nc_ht=scontent-sjc3-1.xx&oh=9a3fe49ceb384bb9dbedad4bb7ed678a&oe=5E95F8EE" 
+                <img src="homepage3.png" 
                 />
               </div>
               <div className="c2-block-content">
@@ -53,7 +53,7 @@ export default () => {
           <div className="c2-block-container">
             <div className="c2-block">
               <div className="block-img">
-                <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/42502732_246464739398907_7035450498668822528_n.png?_nc_cat=1&_nc_sid=6825c5&_nc_ohc=AdgrsfKnl9AAX8bHNtD&_nc_ht=scontent-sjc3-1.xx&oh=0f0181edb832ff4e11150b5dee3d0063&oe=5EA4782F"
+                <img src="homepage4.png"
                 />
               </div>
               <div className="c2-block-content">
--- frontend/components/nav-bar/nav_bar.jsx
+++ frontend/components/nav-bar/nav_bar.jsx
@@ -8,7 +8,7 @@ export default () => {
     <div className="nav-bar">
       <div className="nav-left">
         <img
-          src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/37794079_285442762012286_2170626851641229312_n.png?_nc_cat=1&_nc_sid=6825c5&_nc_ohc=vwGcGTn0yz8AX-x6V4F&_nc_ht=scontent-sjc3-1.xx&oh=6108b7210bebccbcbd6588699a408c7e&oe=5E8ED974"
+          src="nav-bar.png"
           className="nav-logo"
         />
       </div>
